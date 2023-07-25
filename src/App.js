3f1f47e46db871b1ba519730db5414dbf248e27e
irene-nakabugo <irenenakabugo0@gmail.com>
2023-07-25 11:53:54 +0300

    props
    introduction to props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import ProductCard from './ProductCard';
 
 function App() {
   return (
     <div className="App">
-      
+      <ProductCard name="First Bite" img="" price="30" currency="$" />
     </div>
   );
 }
---
+++ src/ProductCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Card,Button } from "react-bootstrap";
+
+{/**utilizing props in the function ProductCard */}
+
+function ProductCard({name,img,price,currency}){
+
+    return(
+            <Card style={{width:'18rem'}}>
+                <Card.Img variant="top" src={img}/>
+                <Card.Body>
+                    <Card.Title>
+                            {name}
+                    </Card.Title>
+                    <Card.Text>
+                        {price}{currency}
+                    </Card.Text>
+                    <Button variant="success">Add to Cart</Button>
+                </Card.Body>
+            </Card>
+    )
+}
+export default ProductCard
